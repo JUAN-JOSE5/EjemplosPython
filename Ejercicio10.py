73a769e5fc45f2b1cbf3d79cebaa8eee6f4beecc
Juan Jose Millan Corzo <Juanmc32145@gmail.com>
2019-08-08 17:33:50 -0500

    10

---
+++ Ejercicio10.py
@@ -0,0 +1,3 @@
+diccionario = {'nombre' : 'Juan Millan', 'edad' : 17, 'curso': 'Programacion De Software' }
+for key in diccionario:
+    print (key,':', diccionario[key])
--- Ejercicio9.py
+++ Ejercicio9.py
@@ -7,15 +7,14 @@ class Empleado:
 
     def calcular_retencion(self):
         retencion= (self.salario_base)*0.10
-        total = self.salario_base-retencion
-        return total
+        return retencion
 
     def mostrar_nombrecompleto(self):
         nombrecomp= self.nombres + " " + self.apellidos
         return nombrecomp
 
     def calcular_salario_neto(self):
-        if(self.salario_base<=828116):
+        if(self.salario_base<=828.116):
             salarioneto= self.salario_base+100000
         else:
             salarioneto=self.salario_base
@@ -25,7 +24,7 @@ emp = Empleado()
 emp.codigo =input("Ingrese el codigo : ")
 emp.nombres=input("Ingrese los nombres : ")
 emp.apellidos=input("Ingrese los apellidos : ")
-emp.salario_base=int(input("Digite el salario base : "))
-print("El salario con la retencion es de : " ,emp.calcular_retencion())
+emp.salario_base=float(input("Digite el salario base : "))
+print("La retencion es de : " ,emp.calcular_retencion())
 print("El nombre completo es : ",emp.mostrar_nombrecompleto())
-print("El salario neto es :" ,emp.calcular_salario_neto())
+print("El salario es :" ,emp.calcular_salario_neto())
